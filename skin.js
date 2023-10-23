42b738aceaf6f01674b685d460da5856fdac7ccc
vink-wedding <119468534+vink-wedding@users.noreply.github.com>
2023-10-23 10:39:14 +0900

    ending12

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.0.6/20004
 // Filename: ?? ?????-?????.ggsk
-// Generated 2023-10-23T10:25:36
+// Generated 2023-10-23T10:38:03
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_catagory', 2, false, { ignoreInState: 0  });
